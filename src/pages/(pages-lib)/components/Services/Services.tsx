68d82f4b9acc546ae3aaea879edb287b86e8e972
SantiagoRicardo <sricard2@ibero.edu.co>
2025-01-14 17:34:36 -0500

    feact(Hero, Services)

--- src/pages/(pages-lib)/components/Services/Services.tsx
+++ src/pages/(pages-lib)/components/Services/Services.tsx
@@ -1,18 +1,7 @@
 import { type FC } from "react";
-import Image from "next/image";
 
-const Hero: FC = () => (
+const Services: FC = () => (
   <section className="relative">
-    <div className="absolute inset-0 z-0">
-      <Image
-        src="/images/sembrado-de-yuca.png"
-        fill
-        priority
-        alt="Hero"
-        className="object-cover contrast-50"
-      />
-    </div>
-
     <div className="relative z-10 flex h-screen flex-col items-center justify-center">
       <h1 className="max-w-5xl text-center font-sans text-6xl font-medium text-white">
         Descubre el sabor auténtico de la yuca con Almidones El Mana
@@ -20,4 +9,4 @@ const Hero: FC = () => (
     </div>
   </section>
 );
-export default Hero;
+export default Services;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,15 @@
+// index.page.tsx
+import React from "react";
 import Hero from "./(pages-lib)/components/Hero";
+import { Services } from "./(pages-lib)/components";
 
-const Home = () => (
-  <>
-    <Hero />
-  </>
-);
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+      <Services />
+    </div>
+  );
+};
 
-export default Home;
+export default HomePage;
