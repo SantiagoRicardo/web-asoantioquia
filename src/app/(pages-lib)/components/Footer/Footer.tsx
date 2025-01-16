23eeba5718dc65026c21f74010b24e1165257da4
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2025-01-16 13:16:04 -0500

    feat(footer): add content

--- src/app/(pages-lib)/components/Footer/Footer.tsx
+++ src/app/(pages-lib)/components/Footer/Footer.tsx
@@ -2,15 +2,56 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <footer className="h-100 relative flex items-end justify-center overflow-hidden bg-[#9FC52E]">
-      <div className="bg-custom-radial h-240 w-240 20 -top-45 absolute z-10 rounded-full"></div>
-      <Image
-        src={"/tree.png"}
-        alt="Tree"
-        width={230}
-        height={230}
-        className="z-20"
-      />
+    <footer className="h-186 items-between relative flex justify-center overflow-hidden bg-[#9FC52E]">
+      <div className="bg-custom-radial h-240 w-240 20 -bottom-85 absolute z-10 rounded-full"></div>
+
+      <div className="p-17.5 z-20 flex w-full justify-between">
+        <section className="flex flex-col justify-between">
+          <div>
+            <h3 className="text-3xl font-semibold text-white">Sedes</h3>
+            <ul className="mt-2 space-y-1 text-lg text-gray-50">
+              <li>Turbo</li>
+              <li>Montería</li>
+              <li>Bajo Cauca</li>
+              <li>Córdoba</li>
+            </ul>
+          </div>
+          <div className="shadow-dark overflow-hidden rounded-2xl">
+            <Image
+              src={"/superservicios.png"}
+              alt="superservicios"
+              width={247}
+              height={131}
+            />
+          </div>
+        </section>
+        <section className="space-y-6 text-end">
+          <div>
+            <h3 className="text-3xl font-semibold text-white">Contacto</h3>
+            <ul className="mt-2 space-y-1 text-lg text-gray-50">
+              <li>asoredcifuturodeantioquia@gmail.com</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-3xl font-semibold text-white">Servicios</h3>
+            <ul className="mt-2 space-y-1 text-lg text-gray-50">
+              <li>Gestión Ambiental </li>
+              <li>Gestión Social</li>
+              <li>Gestión Operativa</li>
+            </ul>
+          </div>
+        </section>
+      </div>
+
+      <div className="absolute z-30 flex h-full w-full items-end justify-center">
+        <Image
+          src={"/tree.png"}
+          alt="Tree"
+          width={230}
+          height={230}
+          className="relative top-6"
+        />
+      </div>
     </footer>
   );
 };
